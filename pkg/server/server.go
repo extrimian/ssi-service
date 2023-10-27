e170d6f69009197bcf9b56e29011ce82f430641b
lucas fm <lfernandez@extrimian.com>
2023-10-27 16:29:51 -0300

    changed waci endpoint from GET to PUT

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -163,7 +163,7 @@ func CredentialsBBSAPI(rg *gin.RouterGroup, service svcframework.Service) (err e
 	credentialsBBSAPI.PUT("/wacioob", credentialsBBSRouter.CreateOOBFromVC)
 	credentialsBBSAPI.GET("", credentialsBBSRouter.ListCredentials)
 	credentialsBBSAPI.GET("/render", credentialsBBSRouter.ListCredentialsWithRender)
-	credentialsBBSAPI.GET("/waci", credentialsBBSRouter.ProcessMessage)
+	credentialsBBSAPI.PUT("/waci", credentialsBBSRouter.ProcessMessage)
 
 	credentialsBBSAPI.PUT("/didcomm/pack", credentialsBBSRouter.PackDIDComm)
 	return
